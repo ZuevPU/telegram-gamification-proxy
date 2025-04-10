620653b7bbd6ffd0513b8a1d064dae566ee7f0e3
Зуев Петр <zuevpu@Gmail.com>
2025-04-10 18:48:14 +0300

    Update submit_user.js

--- api/submit_user.js
+++ api/submit_user.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     return res.status(405).send({ message: 'Only POST allowed' });
   }
 
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycbwUg0_z35KtnD9vd0xvLa5orArM0LtgwcrYTqxt2l6hjuxK_zvGLHTyzxgu0vH_UuQB/exec';
+  const scriptUrl = 'https://script.google.com/macros/s/AKfycbzyEMI5C-6-PKXPCB2xtctctKOYkI_PeYcCt3RA-mtd-5nYQgxk9jGl0uixUkhHJXhi/exec';
 
   try {
     const response = await fetch(scriptUrl, {
