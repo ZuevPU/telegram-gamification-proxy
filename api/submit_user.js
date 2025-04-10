bb6abff7913e69fdfce33d8f56d171ddb78e5462
Зуев Петр <zuevpu@Gmail.com>
2025-04-10 16:43:41 +0300

    Update submit_user.js

--- api/submit_user.js
+++ api/submit_user.js
@@ -1,9 +1,17 @@
 export default async function handler(req, res) {
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycbxiYt-VWJwhZUCOJo4aLBSzdvKvHD2IoGWf_zuO4HewIeNY5YyVUoztkPxN7QkAaI3L/exec';
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Preflight
+  }
 
   if (req.method !== 'POST') {
     return res.status(405).send({ message: 'Only POST allowed' });
   }
+
+  const scriptUrl = 'https://script.google.com/macros/s/AKfycbxiYt-VWJwhZUCOJo4aLBSzdvKvHD2IoGWf_zuO4HewIeNY5YyVUoztkPxN7QkAaI3L/exec';
 
   try {
     const response = await fetch(scriptUrl, {
@@ -13,7 +21,6 @@ export default async function handler(req, res) {
     });
 
     const text = await response.text();
-
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send(text);
   } catch (err) {
