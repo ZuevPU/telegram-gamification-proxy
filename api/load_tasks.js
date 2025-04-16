2e11ab6e10287edb312d2023fcbfa8fa0007d51b
Зуев Петр <zuevpu@Gmail.com>
2025-04-16 22:54:28 +0300

    Update load_tasks.js

--- api/load_tasks.js
+++ api/load_tasks.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   }
 
   try {
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycby0ZaGp-y9RB_WD7hhdeXjyJ57Jz94Jd-rqKmaY-PtEfbOwsyM00Km91vCvDgyIQxAN/exec?action=load_tasks';
+  const scriptUrl = 'https://script.google.com/macros/s/AKfycbzO-qc4Sc-B7BnEGL8fSQUkE6v7rUVL-spPkm9rU5urnA2HKKjsi8zmz7kjGrH5ZBcA/exec?action=load_tasks';
 
     const response = await fetch(scriptUrl);
     const text = await response.text();
