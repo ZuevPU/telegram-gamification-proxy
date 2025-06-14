24be8bfe9afdb625ad4ed9a34e6eccba4bf5c209
Зуев Петр <zuevpu@Gmail.com>
2025-06-14 22:02:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // lib/config.js
-export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwspegZdmoMOzAGWkRFyCKVfBdrLBTCJcf8H0CpRrxdNMeEYguFQHAtWmP7lEjxfJC1/exec';
+export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzjSSb5TAZVupX0FsParQsw2R5bPNgaunOUO6JJZfXAcDeqQ2ASDZ9LeiRL4GvOnWAY/exec';
