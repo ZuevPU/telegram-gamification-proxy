db18f1f8cbe61aeb1e472b7213062783f75269be
Зуев Петр <zuevpu@Gmail.com>
2025-06-14 23:07:43 +0300

    Update buy_lot.js

--- api/buy_lot.js
+++ api/buy_lot.js
@@ -1,4 +1,4 @@
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzjSSb5TAZVupX0FsParQsw2R5bPNgaunOUO6JJZfXAcDeqQ2ASDZ9LeiRL4GvOnWAY/exec';
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzKlFMbvz8vMV2qtsgJTUuqEc2GtjBw11k_KQ8EiOMyr-5Q08r3JuPSeDquYMv3_wo9/exec';
 
 export default async function handler(req, res) {
     const allowedOrigin = 'https://zuevpu.github.io';
